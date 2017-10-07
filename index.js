aa05e407edde57b759b6d197efc78e290ddc22e8
Maxwell Chow <maxchowla@gmail.com>
2017-10-06 17:59:28 -0700

    Chastises users now

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+function UnitHandler(typeconvs) {
+    this.types = typeconvs;
+}
+
+UnitHandler.prototype.addConvs = function(toAdd) {
+    this.types = Object.assign(this.types, toAdd);
+};
+
+UnitHandler.prototype.setStandardConvs = function() {
+    this.types = 
+};
+
 
 module.exports = {
-    
+    UnitHandler
 }
